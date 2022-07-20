aa5e76cb4a9a8a5ff48e4c1eed9f6312994c968b
Sergey Obidin <player181285@gmail.com>
2022-07-20 20:53:45 +0400

    added modified task.js in 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length){ 
+    result = arr1.every((el,i) => el === arr2[i])
+  }
+  else{
+   result = false;
+  }
   return result; // boolean
 }
 
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter(el => el > 0 && el%3 === 0).map(el => el * 10);
 }
