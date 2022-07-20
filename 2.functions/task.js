5bc26f6d2de22ced5d52c41a192ac488fbec618e
Sergey Obidin <player181285@gmail.com>
2022-07-20 17:44:43 +0400

    added modified task.js in 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,51 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0], max = arr[0], sum = 0;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+for (i =0; i<arr.length; i++){
+  sum += arr[i];
+if (arr[i]<min){
+  min = arr[i];
+} 
+if (arr[i]>max) {
+  max = arr[i];
 }
+}
+let avg = sum/arr.length;
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
+}
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = arr.reduce((accum,el) => accum + el,0);
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++){
+     sumOfArr = func(arrOfArr[i]);
+     if (max < sumOfArr){
+      max = sumOfArr
+     }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+  let max = arr[0];
+  let min = arr[0];
+  arr.forEach(el => {
+    if (max < el){
+      max = el
+     }
+     if (min > el){
+      min = el
+     }
+  }); 
+  let result = max - min ;
+  return  result;
+ }
