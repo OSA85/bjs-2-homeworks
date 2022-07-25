1c67250152822559af9690f4f85df08656bf0035
Sergey Obidin <player181285@gmail.com>
2022-07-25 16:00:02 +0400

    added modified task.js in 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
+   this.setSubject;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  // if(this.marks === undefined){ 
+  //   this.marks = [mark];
+  //   } else {
+  //     this.marks.push(mark);
+  //   }
+  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  // if(this.marks === undefined){ 
+  //   this.marks = [...marks];
+  //   } else {
+  //     this.marks.push(...marks);
+  //   }
+  (this.marks === undefined) ? this.marks = [...marks] :  this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((accum, mark) => accum + mark, 0)/this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subjectNamel;
+  this.excluded = reason;
+}
+ 
